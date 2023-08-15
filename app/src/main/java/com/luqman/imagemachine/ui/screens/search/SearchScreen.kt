94205adf465226eea9bf14824b16aa812d7e5dce
Luqman <lqman104@gmail.com>
2023-08-15 10:19:17 +0700

    feat: create search page

---
+++ app/src/main/java/com/luqman/imagemachine/ui/screens/search/SearchScreen.kt
@@ -0,0 +1,12 @@
+package com.luqman.imagemachine.ui.screens.search
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SearchScreen(modifier: Modifier = Modifier) {
+    Column(modifier) {
+
+    }
+}
