c7aabf3e271385a0e5279114255bee36fce54a16
Luqman <lqman104@gmail.com>
2023-08-17 17:05:51 +0700

    feat: add validation on store data

--- domain/src/main/java/com/luqman/imagemachine/domain/usecase/StoreMachineUseCase.kt
+++ domain/src/main/java/com/luqman/imagemachine/domain/usecase/StoreMachineUseCase.kt
@@ -1,9 +1,11 @@
 package com.luqman.imagemachine.domain.usecase
 
 import com.luqman.imagemachine.core.model.Resource
+import com.luqman.imagemachine.core.model.UiText
 import com.luqman.imagemachine.core.model.toUiText
 import com.luqman.imagemachine.data.repository.DataSource
 import com.luqman.imagemachine.data.repository.model.Machine
+import com.luqman.imagemachine.domain.R
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
@@ -14,9 +16,17 @@ class StoreMachineUseCase @Inject constructor(
 
     operator fun invoke(machine: Machine): Flow<Resource<Any>> = flow {
         try {
-            emit(Resource.Loading())
-            repository.store(machine)
-            emit(Resource.Success(null))
+            when {
+                machine.name.isEmpty() -> emit(Resource.Error(UiText.StringResource(R.string.name_empty_error)))
+                machine.type.isEmpty() -> emit(Resource.Error(UiText.StringResource(R.string.type_empty_error)))
+                machine.code.isEmpty() -> emit(Resource.Error(UiText.StringResource(R.string.code_empty_error)))
+                machine.lastMaintain == 0L -> emit(Resource.Error(UiText.StringResource(R.string.last_maintain_empty_error)))
+                else -> {
+                    emit(Resource.Loading())
+                    repository.store(machine)
+                    emit(Resource.Success(null))
+                }
+            }
         } catch (error: Exception) {
             emit(Resource.Error(error.toUiText()))
         }
